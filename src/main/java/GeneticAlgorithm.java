9fcc18b0cfbaf58a86e624f4d36745324c28f3d6
Bartosz Binda <batinex@gmail.com>
2017-03-18 17:25:51 +0100

    Added BiniaryString as a representation to pop

--- src/main/java/GeneticAlgorithm.java
+++ src/main/java/GeneticAlgorithm.java
@@ -3,14 +3,14 @@ import java.util.concurrent.ThreadLocalRandom;
 
 
 public class GeneticAlgorithm {
-    private ArrayList<int[]> populationArray = new ArrayList<>();
+    private ArrayList<String[]> populationArray = new ArrayList<>();
     void initializePopulation(int numNodes, int amountOfPopulations){
         int count = 0;
         while(count != amountOfPopulations) {
-            int[] population = new int[numNodes * numNodes];
+            String[] population = new String[numNodes * numNodes];
             for (int i = 0; i < numNodes * numNodes; i++) {
                 int nodeID = ThreadLocalRandom.current().nextInt(1, numNodes + 1);
-                population[i] = nodeID;
+                population[i] = Integer.toBinaryString(nodeID);
             }
             populationArray.add(population);
             count++;
@@ -18,7 +18,7 @@ public class GeneticAlgorithm {
     }
     void printPopulationArray() {
         for(int i = 0; i < populationArray.size(); i++){
-            int[] arr = populationArray.get(i);
+            String[] arr = populationArray.get(i);
             System.out.print(arr[0]);
             for(int j = 1; j < arr.length; j++){
                 System.out.print(" : " + arr[j]);
